2c35f8738832a84c074aace3810016f74c9b0125
Richard West <r.west@northeastern.edu>
2019-06-17 11:24:39 -0400

    Small fixes to slurm submit.sh script
    -p conflicted with the --partition below it
    Gb0B seemed to be a typo

--- test2/submit.sh
+++ test2/submit.sh
@@ -4,13 +4,12 @@
 #SBATCH --job-name=CHPD 
 ## output specifies the output file name
 #SBATCH --output=CHPD.log
-#SBATCH -p debug
 ## number of nodes
 #SBATCH -N 1
 ##SBATCH --exclusive
 ## Can change partition from general to west
 #SBATCH --partition=west 
-#SBATCH --mem=16GbOB
+#SBATCH --mem=16Gb
 ##SBATCH --time=23:59:59
 #SBATCH --error=error.log
 
