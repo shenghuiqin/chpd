573ce5e8f79cc643c80ebfb1c436653cfc6ec8d1
shenghuiqin <qin.she@husky.neu.edu>
2019-06-26 14:16:36 -0400

    input simplification

--- quick_chpd/input.py
+++ quick_chpd/input.py
@@ -1,6 +1,6 @@
 #Data sources
 database(
-    thermoLibraries = ['primaryThermoLibrary', 'BurkeH2O2','DFT_QCI_thermo'],#['BurkeH2O2','FFCM1(-)','primaryThermoLibrary','DFT_QCI_thermo','CBS_QB3_1dHR'],
+    thermoLibraries = ['primaryThermoLibrary']   # 'BurkeH2O2','DFT_QCI_thermo'],#['BurkeH2O2','FFCM1(-)','primaryThermoLibrary','DFT_QCI_thermo','CBS_QB3_1dHR'],
     reactionLibraries =[],  #[('FFCM1(-)',False),('2005_Senosiain_OH_C2H2',False)],
     seedMechanisms = [],    #['BurkeH2O2inN2'],
     kineticsDepositories = ['training'], 
@@ -23,16 +23,7 @@ species(
     reactive=True,
     structure=SMILES("C1C=CCCCC=1"),
 )
-species(
-    label='CO',
-    reactive=True,
-    structure=SMILES("[C-]#[O+]"),
-)
-species(
-    label='CO2',
-    reactive=True,
-    structure=SMILES("C(=O)=O"),
-)
+
 species(
     label='O2',
     reactive=True,
@@ -46,103 +37,7 @@ species(
     label='N2',
     reactive=False,
     structure=SMILES("N#N")
-)   
-species(
-    label = "CH4",
-    reactive=True,
-    structure = SMILES("C"))
-
-species(
-    label = "C2H6",
-    reactive=True,
-    structure = SMILES("CC"))
-
-species(
-    label = "C3H8",
-    reactive=True,
-    structure = SMILES("CCC"))
-
-species(
-    label = "C4H10",
-    structure = SMILES("CCCC"))
-
-species(
-    label = "C5H12",
-    reactive=True,
-    structure = SMILES("CCCCC"))
-
-species(
-    label = "C6H14",
-    reactive=True,
-    structure = SMILES("CCCCCC"))
-
-species(
-    label = "C2H4",
-    reactive=True,
-    structure = SMILES("C=C"))
-
-species(
-    label = "C3H6",
-    reactive=True,
-    structure = SMILES("CC=C"))
-
-species(
-    label = "C4H8",
-    reactive=True,
-    structure = SMILES("CCC=C"))
-
-species(
-    label = "C5H10",
-    reactive=True,
-    structure = SMILES("CCCC=C"))
-
-species(
-    label = "C6H12",
-    reactive=True,
-    structure = SMILES("CCCCC=C"))
-species(
-    label = "METHYL",
-    reactive=True,
-    structure = SMILES("[CH3]"))
-
-species(
-    label = "ETHYL",
-    reactive=True,
-    structure = SMILES("C[CH2]"))
-
-species(
-    label = "PROPYL",
-    reactive=True,
-    structure = SMILES("CC[CH2]"))
-
-species(
-    label = "BUTYL",
-    reactive=True,
-    structure = SMILES("CCC[CH2]"))
-
-species(
-    label = "PENTYL",
-    reactive=True,
-    structure = SMILES("CCCC[CH2]"))
-
-species(
-    label = "HEXYL",
-    reactive=True,
-    structure = SMILES("CCCCC[CH2]"))
-
-species(
-    label = "BENZYL",
-    reactive=True,
-    structure = SMILES("[CH2]c1ccccc1"))
-
-"""
-    species(
-    label='C7H16',
-    reactive=True,
-    structure=SMILES("CCCCCCC"),
 )
-"""
-
 
 # Reaction systems
 simpleReactor(
@@ -150,10 +45,10 @@ simpleReactor(
     pressure=[(10.0,'bar'),(40.0,'bar')],
     nSims=8,
     initialMoleFractions={
-        "C7H10": 0.00463,
+        "C7H10": 1,
 #	    "C7H16": 0.00463,
-        "O2":    0.10880, # phi=1 means 9.5 O2 per C7H10
-        "N2":     0.88035, # 8.1 times as much N2 as O2
+        "O2":   9.5, # phi=1 means 9.5 O2 per C7H10
+        "N2":    76.95, # 8.1 times as much N2 as O2
         
     },
     terminationRateRatio=0.01,
