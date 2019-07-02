1cd87a52382b2eef427aebeee448fbbdcafaf9e1
shenghuiqin <qin.she@husky.neu.edu>
2019-07-02 16:11:51 -0400

    trial stage

---
+++ trial/submit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## job-name is just how you want the job labelled
+#SBATCH --job-name=trial_stage
+## output specifies the output file name
+#SBATCH --output=stage.log
+## number of nodes
+#SBATCH -N 2
+##SBATCH --exclusive
+## Can change partition from general to west
+#SBATCH --partition=general
+#SBATCH --mem=50Gb
+##SBATCH --time=23:59:59
+#SBATCH --error=error.log
+#SBATCH --exclude=c5003
+
+source activate rmg_env
+python  /home/qin.she/Code/RMG-Py/rmg.py -p input.py
