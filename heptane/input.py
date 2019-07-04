76feaf427b49650efc92992aa84ca53912e00f95
shenghuiqin <qin.she@husky.neu.edu>
2019-07-04 12:19:17 -0400

    heptane simple to get seed mechanism

--- heptane/input.py
+++ heptane/input.py
@@ -1,8 +1,8 @@
 #Data sources
 database(
-    thermoLibraries =['primaryThermoLibrary', 'BurkeH2O2','DFT_QCI_thermo'], #['BurkeH2O2','FFCM1(-)','primaryThermoLibrary','DFT_QCI_thermo','CBS_QB3_1dHR'],
-    reactionLibraries = [('FFCM1(-)',False),('2005_Senosiain_OH_C2H2',False)],
-    seedMechanisms = ['BurkeH2O2inN2'],
+    thermoLibraries =[], # 'FFCM1(-)','primaryThermoLibrary', 'BurkeH2O2','DFT_QCI_thermo','CBS_QB3_1dHR'
+    reactionLibraries = [], # ('FFCM1(-)',False),('2005_Senosiain_OH_C2H2',False)
+    seedMechanisms = [], #'BurkeH2O2inN2'
     kineticsDepositories = ['training'], 
     kineticsFamilies = 'default',
     kineticsEstimator = 'rate rules',
@@ -11,9 +11,8 @@ database(
 # Constraints on generated species
 generatedSpeciesConstraints(
     maximumRadicalElectrons = 2,
-    allowed=['input species','seed mechanisms','reaction libraries'],
-    maximumCarbonAtoms=10,
-    allowSingletO2 = False,
+  # allowed=['input species','seed mechanisms','reaction libraries'],
+    maximumCarbonAtoms=8,
 )
 #this is a test#
 #test local vs#
@@ -38,48 +37,44 @@ species(
     structure=SMILES("N#N")
 )
 
-
-
-
 # Reaction systems
 simpleReactor(
-    temperature=[(700,'K'),(1500,'K')],
+    temperature=[(700,'K'),(1000,'K')],
     pressure=[(20.0,'bar'),(40.0,'bar')],
     nSims=8,
     initialMoleFractions={
         #"C7H10": 1,
         "O2":   11, # phi=1 means 9.5 O2 per C7H10
         "N2":    89.1, # 8.1 times as much N2 as O2
-        "C7H16":1,
-       
+        "C7H16":1,  
     },
-
-    terminationTime=(0.1e0,'s'), 
-    #sensitivity=['C7H16','O2'],
-    #sensitivityThreshold=0.001,
+    terminationTime=(1e0,'s'), 
 )
 
 simulator(
     atol=1e-16,
     rtol=1e-8,
-    sens_atol=1e-6,
-    sens_rtol=1e-4,
 )
 
+
 model(
-        toleranceKeepInEdge=0.0,
-        toleranceMoveToCore=0.5,
-        toleranceInterruptSimulation=0.5,
-        maximumEdgeSpecies=100000,
-        maxNumSpecies=100
-)
-model(
-        toleranceKeepInEdge=0.0,
-        toleranceMoveToCore=0.2,
-        toleranceInterruptSimulation=0.2,
-        maximumEdgeSpecies=100000,
+    toleranceKeepInEdge=0.0001,
+    toleranceMoveToCore=0.01,
+    toleranceInterruptSimulation=0.5,
+    maximumEdgeSpecies=1000
 )
 
+options(
+    units='si',
+    # saveRestartPeriod=(1,'hour'),
+    generateOutputHTML=False,
+    generatePlots=True,
+    saveSimulationProfiles=True,
+    saveEdgeSpecies=False,
+)
+
+
+"""
 uncertainty(
     localAnalysis=False,
     globalAnalysis=False,
@@ -91,15 +86,7 @@ uncertainty(
     pceRunTime=1800,
     logx=True
 )
-
-options(
-    units='si',
-    # saveRestartPeriod=(1,'hour'),
-    generateOutputHTML=False,
-    generatePlots=True,
-    saveSimulationProfiles=True,
-    saveEdgeSpecies=False,
-)
+""" 
 
 #quantumMechanics(
 #    software='mopac',
