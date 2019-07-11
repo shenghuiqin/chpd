bc10b0c4c1d3908798ba845037d3fea50681329e
shenghuiqin <qin.she@husky.neu.edu>
2019-07-11 00:03:42 -0400

    after richard revised

---
+++ 3quick_chpd/submit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## job-name is just how you want the job labelled
+#SBATCH --job-name=CHPD+c5seed
+## output specifies the output file name
+#SBATCH --output=2CHPD.log
+## number of nodes
+#SBATCH -N 1
+##SBATCH --exclusive
+## Can change partition from general to west
+#SBATCH --partition=general
+#SBATCH --mem=150Gb
+## number of cores, should match -n flag in python rmg command below
+#SBATCH -n 12
+##SBATCH --time=23:59:59
+#SBATCH --error=error.log
+#SBATCH --exclude=c5003
+
+source activate rmg_env
+python  /home/qin.she/Code/RMG-Py/rmg.py -p -n 12 input.py
