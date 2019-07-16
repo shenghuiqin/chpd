bdd604384ec611889cdbd8fab5857b4cae6cd702
shenghuiqin <qin.she@husky.neu.edu>
2019-07-15 21:35:10 -0400

    remove deleted files

--- 2heptane_richard/input.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8e597421e1848b761a2734bbaead95182778867c4de74e4327477fc33776261b
-size 3241
--- 3quick_chpd/input.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a657771cc13be22999758988951f165390560d257509640f6d4a34c8c4a1076b
-size 3291
