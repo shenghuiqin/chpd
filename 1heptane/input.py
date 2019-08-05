1f45ccaacd48ed017624efa76e8f758075beadc3
shenghuiqin <qin.she@husky.neu.edu>
2019-08-04 23:01:52 -0400

    using rmg suggested lib

--- 1heptane/input.py
+++ 1heptane/input.py
@@ -1,8 +1,8 @@
 #Data sources
 database(
-    thermoLibraries =['BurkeH2O2','FFCM1(-)','primaryThermoLibrary','DFT_QCI_thermo','CBS_QB3_1dHR'], # 'FFCM1(-)','primaryThermoLibrary', 'BurkeH2O2','DFT_QCI_thermo','CBS_QB3_1dHR'
-    reactionLibraries = [('C6H5_C4H4_Mebel',False),('2005_Senosiain_OH_C2H2',False),('Glarborg/C3', False)], # 
-    seedMechanisms = ['BurkeH2O2inN2','FFCM1(-)','Klippenstein_Glarborg2016',], #
+    thermoLibraries =['BurkeH2O2','Klippenstein_Glarborg2016','thermo_DFT_CCSDTF12_BAC','CBS_QB3_1dHR','DFT_QCI_thermo','primaryThermoLibrary'], # 'FFCM1(-)','primaryThermoLibrary', 'BurkeH2O2','DFT_QCI_thermo','CBS_QB3_1dHR'
+    reactionLibraries = [('2005_Senosiain_OH_C2H2',False),('Glarborg/C3', False)], # 
+    seedMechanisms = ['BurkeH2O2inN2','Klippenstein_Glarborg2016','7.31heptane1Seed'], #
     kineticsDepositories = ['training'], 
     kineticsFamilies = ['default'],
     kineticsEstimator = 'rate rules',
@@ -14,53 +14,46 @@ generatedSpeciesConstraints(
     allowed=['input species','seed mechanisms','reaction libraries'],
     maximumCarbonAtoms=9,
     maximumOxygenAtoms=4,
+    allowSingletO2=True,
 )
 
 # List of species
 species(
-    label='n-heptane',
+    label='C7H16',
     reactive=True,
     structure=SMILES("CCCCCCC"),
 )
-species(
+species(    
     label='O2',
     reactive=True,
-        structure=adjacencyList(
-        """
-        1 O u1 p2 {2,S}
-        2 O u1 p2 {1,S}
-        """),
+    structure=SMILES("[O][O]"),
 )
 species(
     label='N2',
     reactive=False,
     structure=SMILES("N#N")
 )
-species(
-    label='CH4',
-    reactive=True,
-    structure=SMILES("C"),
-)
+
 
 
 
 
 # Reaction systems
 simpleReactor(
-    temperature=[(600,'K'),(1000,'K')],
-    pressure=[(20.0,'bar'),(40.0,'bar')],
+    temperature=[(600,'K'),(1500,'K')],
+    pressure=[(10.0,'bar'),(40.0,'bar')],
     nSims=8,
     initialMoleFractions={
         #"C7H10": 1,
         "O2":   0.10885, # phi=1 means 9.5 O2 per C7H10
         "N2":   0.881684808, # 8.1 times as much N2 as O2
-        "n-heptane":0.00946, #  
-        "CH4": 0.001,   
+        "C7H16":0.00946, #  
+           
     },
     terminationTime = (1.0, 's'),
     terminationRateRatio = 0.01,
     terminationConversion={
-                'n-heptane': 0.9,
+                'O2': 0.4,
         },
 )
 
@@ -72,16 +65,16 @@ simulator(
 
 model(
     toleranceKeepInEdge=0, # No pruning to start
-    toleranceMoveToCore=0.5,
+    toleranceMoveToCore=0.4,
     toleranceInterruptSimulation=1,
     maxNumObjsPerIter=3, 
     terminateAtMaxObjects=True,
-    maxNumSpecies=100, # first stage is until core reaches 100 species
+    maxNumSpecies=200, # first stage is until core reaches 100 species
     filterReactions=True, # should speed up model generation
 )
 
 model(
-    toleranceMoveToCore=0.4,
+    toleranceMoveToCore=0.3,
     toleranceInterruptSimulation=1e8,
     toleranceKeepInEdge=0.01, # Pruning enabled for stage 2
     maximumEdgeSpecies=200000,
