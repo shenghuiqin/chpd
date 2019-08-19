2e4b6908ae05bcbdd4f04be9d38afe9873d3c931
shenghuiqin <qin.she@husky.neu.edu>
2019-08-19 16:32:45 -0400

    change on general partition

--- 3heptane_pdep/submit.sh
+++ 3heptane_pdep/submit.sh
@@ -8,7 +8,7 @@
 #SBATCH -N 1
 ##SBATCH --exclusive
 ## Can change partition from general to west
-#SBATCH --partition=west
+#SBATCH --partition=general
 #SBATCH --mem=120Gb
 ## number of cores, should match -n flag in python rmg command below
 #SBATCH -n 12
