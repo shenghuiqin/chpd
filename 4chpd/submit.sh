36312cf2803da47cb063c62b3c3ac78a660903ff
shenghuiqin <qin.she@husky.neu.edu>
2019-08-21 10:17:54 -0400

    change cores to 6

--- 4chpd/submit.sh
+++ 4chpd/submit.sh
@@ -11,10 +11,10 @@
 #SBATCH --partition=general
 #SBATCH --mem=120Gb
 ## number of cores, should match -n flag in python rmg command below
-#SBATCH -n 12
+#SBATCH -n 6
 ##SBATCH --time=23:59:59
 #SBATCH --error=error.log
 #SBATCH --exclude=c5003
 
 source activate rmg_env
-python  /home/qin.she/Code/RMG-Py/rmg.py -p -n 12 input.py
+python  /home/qin.she/Code/RMG-Py/rmg.py -p -n 6 input.py
